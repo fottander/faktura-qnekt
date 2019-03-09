252d2a6268ace77cb13ceb8b899c1ee8bcf9f3ef
felixottander <felix.ottander@yahoo.com>
2019-03-09 10:29:22 +0100

    oijo

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const index = () => (
   <div>
     <Head>
       <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
       <link rel="stylesheet" href="/static/base.css" />
       <title>My next app</title>
